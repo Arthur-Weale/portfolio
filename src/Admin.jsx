4303f5851ff2c2e4630e32bf6a95bdb3c10541f0
Arthur Weale <ajayrossy340@gmail.com>
2025-10-18 03:40:02 +0200

    Added admin page components

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,12 +1,21 @@
+import AddProjectButton from "./view/components/ui/AddProjectButton";
+import AddProject from "./view/components/ui/AddProjectForm";
+import ProjectCardAdmin from "./view/components/ui/ProjectCardAdmin";
 
-
-function Admin(){
+function Admin() {
     return (
-        <>
-            <h1>Project Manager</h1>
-        </>
+    <>
+        <h1>Project Manager</h1>
+        <div className="panel-container">
+        <div className="left-panel">
+            <AddProjectButton />
+            <AddProject/>
+            <ProjectCardAdmin/>
+        </div>
+        <div className="right-panel"></div>
+        </div>
+    </>
     );
 }
 
-
-export default Admin; 
+export default Admin;
