ddaf646048bfb52a70a77e8d83963ec9132edbed
Arthur Weale <ajayrossy340@gmail.com>
2025-10-18 03:40:20 +0200

    Created the add project button component

---
+++ src/view/components/ui/AddProjectButton.jsx
@@ -0,0 +1,12 @@
+import { IoIosAddCircleOutline } from "react-icons/io";
+
+function AddProjectButton() {
+    return (
+        <button className="add-project-btn">
+            <IoIosAddCircleOutline size={18} />
+            Add Project
+        </button>
+    );
+}
+
+export default AddProjectButton;
