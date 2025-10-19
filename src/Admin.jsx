a6daef5eb115e4c78f38d689ad7502f61151f330
Arthur Weale <ajayrossy340@gmail.com>
2025-10-19 03:48:27 +0200

    Implemented form popup on click

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import AddProjectButton from "./view/components/ui/AddProjectButton";
 import AddProject from "./view/components/ui/AddProjectForm";
 import ProjectCardAdmin from "./view/components/ui/ProjectCardAdmin";
 
 function Admin() {
+    const [isFormActive, setIsFormActive] = useState(false);
     return (
     <>
         <h1>Project Manager</h1>
         <div className="panel-container">
         <div className="left-panel">
-            <AddProjectButton />
-            <AddProject/>
+            <AddProjectButton toggleForm={()=> setIsFormActive(prev => !prev)} />
+            <AddProject isActive={isFormActive}/>
             <ProjectCardAdmin/>
         </div>
         <div className="right-panel"></div>
