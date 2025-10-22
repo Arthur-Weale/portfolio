04518837960bd16cb8b0fbe0a8e28edb68ce280d
Arthur Weale <ajayrossy340@gmail.com>
2025-10-22 03:44:21 +0200

    Added dynamic form switching logic

--- src/view/components/ui/AddProjectButton.jsx
+++ src/view/components/ui/AddProjectButton.jsx
@@ -1,8 +1,12 @@
 import { IoIosAddCircleOutline } from "react-icons/io";
+import AddProject from "./AddProjectForm";
 
-function AddProjectButton() {
+function AddProjectButton({toggleForm}) {
     return (
-        <button className="add-project-btn">
+        <button 
+        className="add-project-btn"
+        onClick={toggleForm}
+        >
             <IoIosAddCircleOutline size={18} />
             Add Project
         </button>
