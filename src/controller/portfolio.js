b8cb580bce0aae9d629c008fcd6f2df6f4ece354
Arthur Weale <ajayrossy340@gmail.com>
2025-10-25 03:04:14 +0200

    Fixed why image url is not in the database

--- src/controller/portfolio.js
+++ src/controller/portfolio.js
@@ -4,21 +4,25 @@ import projectSchema from "../model/schema.js";
 
 await initDb();
 
+// Delete the model if it already exists (only in dev!)//Remove in production
+//delete mongoose.connection.models['Project']; //Deletes the model from cache.
 const ProjectModel = mongoose.model("Project", projectSchema);
 
 const insertProject = async(formData)=>{
     try {
         await initDb(); //Initialises the database connection
+        console.log("Inserting project:", formData);
         const projectModel = await ProjectModel.create({ //Creates a project and validates using the schema.
             name: formData.name,
             description: formData.description,
             live: formData.live,
             github: formData.github,
-            //image_Url: addProject.image_Url,
+            image:formData.image,
         });
+        console.log("Inserted project:", projectModel);
         console.log("Connected DB:", mongoose.connection.name); //Logs the connection name
         console.log("Cluster Host:", mongoose.connection.host);
-        console.log("live url:", formData.live)
+        console.log("live url:", formData.image)
         return projectModel;
     } catch (error) {
         console.log(error)
