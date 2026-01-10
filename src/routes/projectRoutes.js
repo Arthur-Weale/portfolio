758b75fffbcb61e9d61b6f7c07ff244f21e919b5
Arthur Weale <ajayrossy340@gmail.com>
2026-01-10 02:03:17 +0200

    Implementated the update and delete route handlers

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,7 +1,16 @@
 import express from "express";
-import insertProject from "../controller/portfolio.js";
+import { insertProject, getProjects, deleteProject, updateProject } from "../controller/portfolio.js";
 
 const router = express.Router();
+
+router.get("/projects", async (req, res)=>{
+    try {
+        const response = await getProjects(req.userId);
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 router.post("/project", async (req, res)=> { //Route handler function handles the insertion of project throught a post request call.
     try {
@@ -11,6 +20,27 @@ router.post("/project", async (req, res)=> { //Route handler function handles th
     } catch (error) {
         console.log(error)
     }
+})
+
+    router.put("edit/:id", async (req, res) => {
+    try {
+        const idToEdit = req.params.id;
+        const response = await updateProject(idToEdit);
+        res.status(200).json(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.delete("delete/:id", async (req, res) => {
+    try {
+        const idToDelete = req.params.id;
+        const response = await deleteProject(idToDelete);
+        res.status(200).json(response)
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 export default router;
