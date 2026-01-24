59ac27f06d57f51c2d1e4857d142a75a76b8d67f
Arthur Weale <ajayrossy340@gmail.com>
2026-01-25 01:37:18 +0200

    Removed redundant code

--- src/view/components/ui/ProjectCard.jsx
+++ src/view/components/ui/ProjectCard.jsx
@@ -1,108 +1,52 @@
-// import "./App.css";
-import {
-SiHtml5,
-SiCss3,
-SiJavascript,
-SiTailwindcss,
-SiNextdotjs,
-SiPostgresql,
-SiReact,
-SiTypescript,
-SiNodedotjs,
-} from "react-icons/si";
+import { useEffect, useState } from "react";
 
-const projectCardData = [
-{
-    name: "Intellitrade(In Progress)",
-    imageUrl: "/projects/intellitrade.webp",
-    imageAlt: "intellitrade app ui",
-    summary:
-    "A full-stack web application that allows traders to connect broker accounts, configure automated trading bots, and track performance in real-time.",
-    github: "https://github.com/Arthur-Weale/intellitrade.git",
-    live: "https://intellitrade-nine.vercel.app",
-    icons: [
-    {
-        iconName: "SiNextdotjs",
-        iconColor: "#000000",
-    },
-    {
-        iconName: "SiTypescript",
-        iconColor: "#3178C6",
-    },
-    {
-        iconName: "SiReact",
-        iconColor: "#61DAFB",
-    },
-    {
-        iconName: "SiNodedotjs",
-        iconColor: "#339933",
-    },
-    {
-        iconName: "SiPostgresql",
-        iconColor: "#336791",
-    },
-    {
-        iconName: "SiTailwindcss",
-        iconColor: "#06B6D4",
-    },
-    ],
-},
-{
-    name: "Wanderwise",
-    imageUrl: "/projects/wanderwise.webp",
-    imageAlt: "wanderwise app ui",
-    summary:
-    "Wanderwise is a small travel planning app built with React and Vite. It lets users discover destinationsand preview trips with images and information.",
-    github: "https://github.com/Arthur-Weale/wanderwise.git",
-    live: "https://wanderwise-explorer.netlify.app/",
-    icons: [
-    {
-        iconName: "SiHtml5",
-        iconColor: "#E34F26",
-    },
-    {
-        iconName: "SiJavascript",
-        iconColor: "#F7DF1E",
-    },
-    {
-        iconName: "SiCss3",
-        iconColor: "#1572B6",
-    },
-    ],
-},
-];
+async function getProjectCardData(){
+  const url = "http://localhost:3000/projects"
 
-const iconMap = {
-SiNextdotjs,
-SiTypescript,
-SiReact,
-SiNodedotjs,
-SiPostgresql,
-SiTailwindcss,
-SiHtml5,
-SiJavascript,
-SiCss3,
-};
+  try {
+    console.log("Hey there dumb dumb this function is working");
+    const response = await fetch(url);
+    console.log(response)
+    const result = await response.json();
+    console.log(result)
+    return result;
+  } catch (error) {
+    console.log('Failed to get projecs', error)
+    return []
+  }
+}
 
 function ProjectCard(){
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      console.log("Hey there dumb dumb");
+      const data = await getProjectCardData();
+      console.log(data)
+      setProjects(data);
+    }
+    fetchData()
+  }, [])
+
     return(
 <section id="projects" className="projects">
             <div className="subheading-container">
               <h2 className="projects-header">Projects</h2>
             </div>
             <div className="projects-section">
-              {projectCardData.map((card) => (
-                <div className="project1" key={card.name}>
+              {projects.map((card) => (
+                <div className="project1" key={card._id}>
                   <div className="project-image">
                     <img
-                      src={card.imageUrl}
+                      src={card.image}
                       alt={card.imageAlt}
                       loading="lazy"
                     />
                   </div>
                   <div className="project-description">
                     <h2>{card.name}</h2>
-                    <p>{card.summary}</p>
+                    <p>{card.description}</p>
                     <div className="action-btn">
                       <button
                         onClick={() =>
@@ -127,7 +71,7 @@ function ProjectCard(){
                         GitHub
                       </button>
                     </div>
-                    <div className="tech-icons">
+                    {/* <div className="tech-icons">
                       {card.icons.map((icon) => {
                         const IconComponent = iconMap[icon.iconName];
                         return (
@@ -137,7 +81,7 @@ function ProjectCard(){
                           />
                         );
                       })}
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               ))}
