1c803aa984766b25c957525592b337055a3d60b0
Arthur Weale <ajayrossy340@gmail.com>
2026-01-25 01:37:45 +0200

    Removed dummy data

--- src/view/components/ui/ProjectCardAdmin.jsx
+++ src/view/components/ui/ProjectCardAdmin.jsx
@@ -1,45 +1,59 @@
 import { FaEdit } from "react-icons/fa";
 import { FaArchive } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { useEffect, useState } from "react";
 
+async function handleDelete(deletedId){
+  const url = `http://localhost:3000/delete/${deletedId}`
+  const response = await fetch(url, { 
+    method: "DELETE",
+    // headers: {
+    //   "Content-Type": "Application/Json"
+    // }
+  })
 
-const projectCardData = [
-  {
-    name: "Intellitrade(In Progress)",
-    imageUrl: "/projects/intellitrade.webp",
-    imageAlt: "intellitrade app ui",
-    summary:
-      "A full-stack web application that allows traders to connect broker accounts, configure automated trading bots, and track performance in real-time.",
-  },
-  {
-    name: "Wanderwise",
-    imageUrl: "/projects/wanderwise.webp",
-    imageAlt: "wanderwise app ui",
-    summary:
-      "Wanderwise is a small travel planning app built with React and Vite. It lets users discover destinationsand preview trips with images and information.",
-  },
-];
+  if(!response.ok) throw new Error("failed to delete project")
+}
 
 function ProjectCardAdmin() {
+  const [project, setProjects] = useState([]);
+  const url = "http://localhost:3000/projects";
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log("Hey there dumb dumb this function is working");
+      const response = await fetch(url);
+      const data = await response.json()
+      setProjects(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <section id="projects" className="projects-admin">
       <div className="subheading-container">
         <h2 className="projects-header-admin">Projects List</h2>
       </div>
       <div className="projects-section">
-        {projectCardData.map((card) => (
-          <div className="project-admin" key={card.name}>
+        {project.map((card) => (
+          <div className="project-admin" key={card._id}>
             <div className="admin-project-image">
-              <img src={card.imageUrl} alt={card.imageAlt} loading="lazy" />
+              <img src={card.image} alt={card.imageAlt} loading="lazy" />
             </div>
             <div className="project-description-admin">
               <h2>{card.name}</h2>
-              <p>{card.summary}</p>
+              <p>{card.description}</p>
             </div>
             <div className="card-controls">
               <FaEdit size={19} />
               <FaArchive />
-              <MdDelete size={20} />
+              <MdDelete size={20} onClick={async() => {
+                try {
+                  await handleDelete(card._id)
+                } catch (error) {
+                  console.error(error.message)
+                }
+                }} />
             </div>
           </div>
         ))}
