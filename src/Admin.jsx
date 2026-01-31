bde23c3ff65bd486060ace8c07a6d07976347d8e
Arthur Weale <ajayrossy340@gmail.com>
2026-01-31 02:29:16 +0200

    Added popup and overlay

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -5,16 +5,30 @@ import ProjectCardAdmin from "./view/components/ui/ProjectCardAdmin";
 
 function Admin() {
     const [isFormActive, setIsFormActive] = useState(false); //Use state is in parent since two sibling components will share the state.
-    return <>
-        <h1>Project Manager</h1> 
-        <div className="panel-container">
-            <div className="left-panel">
-                <AddProjectButton toggleForm={()=> setIsFormActive(prev => !prev)} /> 
-                <AddProject isActive={isFormActive}/>
-                <ProjectCardAdmin/>
-            </div>
+    return (
+    <>
+    <div className={`panel-container ${isFormActive ? "active" : ""}`}>
+        <h1>Project Manager</h1>
+        <AddProjectButton
+        toggleForm={() => setIsFormActive((prev) => !prev)}
+        />
+        <div className="left-panel">
+        <ProjectCardAdmin />
         </div>
-    </>;
+    </div>
+    {isFormActive && (
+        <div className="overlay"
+        onClick={()=>{setIsFormActive(false)}}>
+        <div
+            className="modal"
+            onClick={(e) => e.stopPropagation()} // prevent modal click from closing
+        >
+            <AddProject />
+        </div>
+        </div>
+    )}
+    </>
+);
 }
 
 export default Admin;
