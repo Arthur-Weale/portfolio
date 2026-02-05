cc8835f25aeda49eb3c7e2f8c29a3832b7b7bcc5
Arthur Weale <ajayrossy340@gmail.com>
2026-02-05 04:07:33 +0200

    Added header to admin page

--- src/view/components/ui/AddProjectForm.jsx
+++ src/view/components/ui/AddProjectForm.jsx
@@ -76,71 +76,74 @@ function AddProject({isActive}){
 }
 
   return (
-    <form
-      className={`project-form ${isActive ? "active" : ""}`}
-      onSubmit={handleSubmit}
-    >
-      <label htmlFor="name">Project Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="description">Description</label>
-      <input
-        type="text"
-        id="description"
-        name="description"
-        value={formData.description}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="live">Live Url</label>
-      <input
-        type="text"
-        id="live"
-        name="live"
-        value={formData.live}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="github">Github Url</label>
-      <input
-        type="text"
-        id="github"
-        name="github"
-        value={formData.github}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="image">Image</label>
-      <input
-        type="file"
-        accept="image/*"
-        id="image"
-        name="image"
-        onChange={handleChange}
-      />
-
-      <label htmlFor="live">Tech Stack</label>
-      <input
-        type="text"
-        id="stack"
-        name="stack"
-        value={formData.stack}
-        onChange={handleChange}
-      />
-      <button
-        className="add-project-btn"
-        type="submit"
-        onClick={() => console.log("i have been clicked")}
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className={`project-form ${isActive ? "active" : ""}`}
       >
-        Add Project
-      </button>
-    </form>
+        <h3>Add New Project</h3>
+        <label htmlFor="name">Project Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="live">Live Url</label>
+        <input
+          type="text"
+          id="live"
+          name="live"
+          value={formData.live}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="github">Github Url</label>
+        <input
+          type="text"
+          id="github"
+          name="github"
+          value={formData.github}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="image">Image</label>
+        <input
+          type="file"
+          accept="image/*"
+          id="image"
+          name="image"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="live">Tech Stack</label>
+        <input
+          type="text"
+          id="stack"
+          name="stack"
+          value={formData.stack}
+          onChange={handleChange}
+        />
+        <button
+          className="add-project-btn"
+          type="submit"
+          onClick={() => console.log("i have been clicked")}
+        >
+          Add Project
+        </button>
+      </form>
+    </div>
   );
 }
 export default AddProject;
